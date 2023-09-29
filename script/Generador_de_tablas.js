69d69a76a69f28a0acee9d5ec7a930b558648e49
GiovanniPeirone <134148500+GiovanniPeirone@users.noreply.github.com>
2023-09-29 12:51:49 -0300

    .ajgd
    ´wor

--- script/Generador_de_tablas.js
+++ script/Generador_de_tablas.js
@@ -1,39 +1,14 @@
-const z = '<input type="text">'
-document.write(z)
+const tab = `<table style="width:100%" class="TextoBlanco">
+                <tr>
+                    <th>Palabra</th>
+                    <th>Definicion</th>
+                    <th>Ejemplo</th>
+                </tr>
+            </table>`
+document.write(tab);
+
+<button onclick="functionAEjecutar()">Click</button>
 
 
-
-
-
-/*class tabla{
-    constructor(tabla){
-        let tab = tabla
-    } 
-}
-
-
-class filas{
-    constructor(Palabra,Definicion,Ejemplo){
-        const Palabra = Palabra;
-        const Definicion = Definicion;
-        const Ejemplo = Ejemplo;
-    }
-}
  
 
-let opcion = prompt("+1 para generar tabla +0 para no generar nada")
-if(opcion == "+1"){
-    tab = new tabla(`<table style="width:100%" class="TextoBlanco">
-                            <tr>
-                                <th>Palabra</th>
-                                <th>Definicion</th>
-                                <th>Ejemplo</th>
-                            </tr>
-                        </table>`
-    )
-    /*palabra = new palabra(`<input type="text">`)
-    document.write(tabla);
-} else{
-    alert(":(");
-}
-*/
