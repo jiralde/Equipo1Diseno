0da48ec082999cc587236f8a9d09f228eac6dfe4
giovanni-peirone <134148500+GiovanniPeirone@users.noreply.github.com>
2023-10-19 22:03:35 -0300

    Giovanni 19/11/23
    + tabla con filas generables

--- script/Generador_de_tablas.js
+++ script/Generador_de_tablas.js
@@ -1,36 +1,18 @@
-function generadorDeFilas(){
-    const fila =+ `<tr>
-                        <td><input type="text"></td>
-                        <td><input type="text"></td>
-                        <td><input type="text"></td>
-                        <td><input type="text"></td>
-                    </tr>`
-    document.write(tabla)                      
+const agregarFila = () => {
+    document.getElementById('tablaprueba').insertRow(-1).innerHTML = '<td><input type="text"></td><td><input type="text"></td><td><input type="text"></td><td><input type="text"></td>';
+  }
+  
+  const eliminarFila = () => {
+    const table = document.getElementById('tablaprueba');
+    const rowCount = table.rows.length;
+    
+    if (rowCount <= 1){
+      alert('No se puede eliminar el encabezado');
+    }else{
+      table.deleteRow(rowCount -1);
+    }
 }
 
-const fila = `
-`;
-
-
-const tabla = `<table>
-                    <tr>
-                        <th>Lenguaje</th>
-                        <th>Funcion</th>
-                        <th>Definicion</th>
-                        <th>Ejemplo</th>
-                    </tr>
-                   
-                    ${fila}
-
-                </table>`;
-document.write(tabla);
-
-
-
-/*<tr>
-                    <td><input type="text"></td>
-                    <td><input type="text"></td>
-                    <td><input type="text"></td>
-                    <td><input type="text"></td>
-</tr>
-*/
+const guardarT = () => {
+    document.write(table)
+}
